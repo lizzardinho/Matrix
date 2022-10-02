0d81868fdbdd972d23ea26bf87fbe1d8038231c3
lizzardinho <banjolieb@gmail.com>
2022-10-02 22:29:29 +0200

    first

---
+++ script.js
@@ -0,0 +1,39 @@
+/*
+
+INSPO 
+
+https://www.freefonts.io/wp-content/uploads/2017/07/elixia-cover.jpg
+
+fonts to try: elixia, exan, high tide, skyer, 
+
+
+
+requirements:
+— overall should be dark with neon/bright accents
+— 1 cyberpunk-y display accent font + 1 sans or mono font
+— site name + nav tree + user menu
+— background should be inspired by background inspo below
+— 1+ cut off corner (e.g., the 45-degree angles in layout inspo!!! below)
+
+bonus: 
++5 points for including a keyframe animation somewhere
++5 points for paragraph blurb and staff images
+
+notes:
+some things to consider: cyberpunk tends to look really busy. it doesn't have to be, but the settings are almost always super busy cities. dark. lots of lights. consider playing with glows. sharp fonts and sharp angles. lots of detail. semi-transparent elements overlapping. 
+
+inspiration:
+colors/glows: https://dribbble.com/shots/15160875-Player-UI
+neons/borders/glows: https://dribbble.com/shots/15700318-Enef-NFT-Marketplace-Website
+vibes/setting: https://www.pinterest.com/pin/519462138269818316/
+vibe + layout inspo: https://dribbble.com/shots/14946026-Cyberpunk-2077-Ui-Design-Concept
+layout inspo: https://dribbble.com/shots/14999548-Nera-Sunglasses-Ui-Ux-Design-Concept
+layout inspo!!!: https://dribbble.com/shots/9875502-Project-Sivappu-Alternative-front-end-for-YouTube
+background inspo: https://unsplash.com/photos/yVUQlyRlJSw
+
+additional font inspo: https://discord.com/channels/831903900478734338/837897460860059678/861062290828427264
+
+
+
+
+*/ 
